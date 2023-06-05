8759eb0f4b4bb9a2229931d0ee0b78e25cbacb7d
sylvieshimwauwase <slvshimwa@gmail.com>
2023-06-05 23:26:40 +0200

    creating new sentence

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str.index("object"):str.index("object")+20
+print(str)
