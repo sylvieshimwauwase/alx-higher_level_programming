adb5fad1e34c03e19ad00c1846cf2104b5b039a9
sylvieshimwauwase <slvshimwa@gmail.com>
2023-06-08 20:04:46 +0200

    magic calculator

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    add, sub = getattr(__import__('magic_calculation_102', fromlist=['add', 'sub']), 'add', 'sub')
+    from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
