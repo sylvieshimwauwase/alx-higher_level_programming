a57e654cb3de8071ac788150d17e3cbcf0e4a907
sylvieshimwauwase <slvshimwa@gmail.com>
2023-06-08 14:39:01 +0200

    printing number of arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    len_args = len(sys.argv) - 1
+    if len_args == 1:
+        print("{} argument".format(len_args))
+
+    elif len_args == 0:
+        print("{} arguments".format(len_args))
+    else:
+       print("{} arguments".format(len_args))
+
+    if len_args >= 1:
+        len_args = 0
+        for args in sys.argv:
+            if len_args != 0:
+                print("{}: {}".format(len_args, args))
+            len_args += 1
