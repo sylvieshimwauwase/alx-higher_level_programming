42450c7a5be7734247e879f7c2c5ccf444754127
sylvieshimwauwase <slvshimwa@gmail.com>
2023-06-08 14:48:54 +0200

    printing number of arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,11 +4,11 @@ if __name__ == "__main__":
     len_args = len(sys.argv) - 1
 
     if len_args == 0:
-        print("{} arguments".format(len_args))
+        print("{} arguments.".format(len_args))
     elif len_args == 1:
-        print("{} argument".format(len_args))
+        print("{} argument:".format(len_args))
     else:
-       print("{} arguments".format(len_args))
+       print("{} arguments:".format(len_args))
 
     if len_args >= 1:
         len_args = 0
