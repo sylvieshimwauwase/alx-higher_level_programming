a1709e15bc7d2ef7bf969124c602aa59eeb95e59
sylvieshimwauwase <slvshimwa@gmail.com>
2023-06-12 11:12:14 +0200

    returning string tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    a1, a2 = tuple_a[:2] + (0, 0)[:2 - len(tuple_a)]
-    b1, b2 = tuple_b[:2] + (0, 0)[:2 - len(tuple_b)]
+    a1, a2 = tuple_a[:2]
+    b1, b2 = tuple_b[:2]
 
     result = (a1 + b1, a2 + b2)
 
