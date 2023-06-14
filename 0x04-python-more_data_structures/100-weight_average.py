533d82ec10b45d47deea94d1e1e87f0e9298fd5d
sylvieshimwauwase <slvshimwa@gmail.com>
2023-06-14 22:57:47 +0200

    average of all integers

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    tot_score = 0
+    tot_weight = 0
+    for score, weight in my_list:
+        tot_score += score * weight
+        tot_weight += weight
+        return (tot_score / tot_weight)
+
+
+my_list = [(1, 2), (2, 1), (3, 10), (4, 2)]
+# = ((1 * 2) + (2 * 1) + (3 * 10) + (4 * 2)) / (2 + 1 + 10 + 2)
+result = weight_average(my_list)
+print("Average: {:0.2f}".format(result))
