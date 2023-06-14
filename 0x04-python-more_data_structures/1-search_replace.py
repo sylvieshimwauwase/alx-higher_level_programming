7bf31da4e0dee4dc61fbc45fd25558d3b9edc93b
sylvieshimwauwase <slvshimwa@gmail.com>
2023-06-14 13:03:15 +0200

    search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    return [list(map(lambda x: 89 if x == 2 else x, my_list))]
+    return list(map(lambda x: replace if x == search else x, my_list))
