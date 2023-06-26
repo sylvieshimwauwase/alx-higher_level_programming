cdbe5e33d64ba2cf49bffb8779fde24ade19b461
sylvieshimwauwase <slvshimwa@gmail.com>
2023-06-26 15:22:56 +0200

    printing elements

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -10,6 +10,6 @@ def safe_print_list(my_list=[], x=0):
                 break
         print()
         return count
-    except TypeError:
+    except IndexError:
         print("Error: List contains unsupported elements.")
         return count
