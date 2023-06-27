de8276caa6c87e26543079d9366d6d7bdbbb7aea
sylvieshimwauwase <slvshimwa@gmail.com>
2023-06-27 21:13:29 +0200

    classes and objects in Python

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""Representing square"""
+
+
+class Square:
+    """Square class representation
+    Attributes:
+        __size (int): size of square
+    """
+
+    def __init__(self, size=0):
+        """new instances of class square
+
+        Args:
+            size (int, optional): size of the square (default 0)
+
+        Raises:
+            TypeError: if size is integer
+            ValueError: if size is less than 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    @property
+    def size(self):
+        """Retrieving size of square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        Calculates and returns area of square
+
+        Returns:
+            int: area of the square
+        """
+
+        return self.__size ** 2
+
+    def my_print(self):
+        """
+        prints square using '#' character
+        if size is 0 prints an empty line
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                print("#" * self.__size)
