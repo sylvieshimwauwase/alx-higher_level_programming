6fc686df25c47345922b9864b0425d2a8ccc6114
sylvieshimwauwase <slvshimwa@gmail.com>
2023-06-27 19:15:06 +0200

    classes and objects in Python

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""Square representation"""
+
+
 class Square:
+    """Representing Square"""
+
     def __init__(self, size):
+        """Initializing square class
+        Args: size - size of quare
+        """
+
         self.__size = size
