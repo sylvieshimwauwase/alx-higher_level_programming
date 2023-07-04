c6416d8f317829aca2056d6a6b527d080bf901ac
pro <pro@pop-os.localdomain>
2023-07-04 11:19:43 +0200

    Python Test Driven Development

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,23 @@
+#!/bin/usr/python3
+"""Function that prints the name"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Prints "My name is <first_name> <last_name>">
+
+    Args:
+        first_name (str): the first name
+        last_name (str): the last name
+
+    Raises:
+        TypeError: If either of first_name or last_name are nor strings.
+    """
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
