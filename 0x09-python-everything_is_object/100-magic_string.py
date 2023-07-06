df3a0132ee2fec8f6be4fbe950486ac43725d009
sylvieshimwauwase <slvshimwa@gmail.com>
2023-07-06 21:47:40 +0200

    Everything is object in Python

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    magic = "BestSchool"
-    return getattr('\n'.join, 'join')(magic * i for i in range(1, 6))
+    magic_string.count = getattr(magic_string, 'count', 0) + 1
+    return ", ".join(["BestSchool" for i in range(magic_string.count)])
--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -3,9 +3,7 @@ from typing import Any
 
 
 class LockedClass:
+    """
+    preventing from instantiating new LockedClass
+    """
     __slots__ = ['first_name']
-
-    def __setattr__(self, name, value):
-        if not hasattr(self, name) and name != 'first_name':
-            raise AttributeError("Cannot create new instance attribute")
-        super().__setattr__(name, value)
