b4243e20872f5a8cb6930b5c3584da36fe3d4ba5
sylvieshimwauwase <slvshimwa@gmail.com>
2023-07-12 15:14:33 +0200

    Python Inheritance

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""function that returns same class or inherrit from"""
+
+
+def is_kind_of_class(obj, a_class):
+    """object is an instance of, or instance of a class that inherited from"""
+
+    return isinstance(obj, a_class)
