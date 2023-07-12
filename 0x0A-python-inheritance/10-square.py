17adeca86628054d689b9efa7c17992ea7a02ad0
sylvieshimwauwase <slvshimwa@gmail.com>
2023-07-12 17:16:41 +0200

    Python Inheritance

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -9,23 +9,10 @@ class Square(Rectangle):
         Initialize a Square object with the given size.
 
         Args:
-        - size: The size of the square.
-
-        Raises:
-        - TypeError: If size is not an integer.
-        - ValueError: If size is less than or equal to 0.
+        - size (int): The size of the square.
         """
 
         self.__size = size
         self.integer_validator("size", size)
         super().__init__(size, size)
 
-    def __str__(self):
-        """
-        Return the square description.
-
-        Returns:
-        - A string representing the square description.
-        """
-
-        return f"[Square] {self.__size}/{self.__size}"
