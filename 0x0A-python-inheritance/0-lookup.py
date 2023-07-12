a37871db6fdde9ed72a3ab8354d430bc3970a936
sylvieshimwauwase <slvshimwa@gmail.com>
2023-07-12 13:51:56 +0200

    Python Inheritance

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -4,15 +4,3 @@ def lookup(obj):
 
     attr = dir(obj)
     return attr
-
-class MyClass1(object):
-    pass
-
-class MyClass2(object):
-    my_attr1 = 3
-    def my_meth(self):
-        pass
-
-print(lookup(MyClass1))
-print(lookup(MyClass2))
-print(lookup(int))
