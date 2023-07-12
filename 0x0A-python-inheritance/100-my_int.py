8fc4cce1282765dd2663a73ab6f1ccfcf8a96ee7
sylvieshimwauwase <slvshimwa@gmail.com>
2023-07-12 17:27:31 +0200

    Python Inheritance

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -3,32 +3,15 @@
 
 
 class MyInt(int):
+    """inverting int operations"""
     def __eq__(self, other):
         """
         Override the == operator.
-
-        Invert the behavior of the == operator.
-
-        Args:
-        - other: The object to compare with.
-
-        Returns:
-        - True if the objects are not equal.
-        - False if the objects are equal.
         """
         return super().__ne__(other)
 
     def __ne__(self, other):
         """
         Override the != operator.
-
-        Invert the behavior of the != operator.
-
-        Args:
-        - other: The object to compare with.
-
-        Returns:
-        - True if the objects are equal.
-        - False if the objects are not equal.
         """
         return super().__eq__(other)
