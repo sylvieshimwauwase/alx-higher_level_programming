10d10cc10debdf8a230e9b06396238a18ca34da1
sylvieshimwauwase <slvshimwa@gmail.com>
2023-07-12 15:32:04 +0200

    Python Inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Geometry module"""
+
+
+class BaseGeometry:
+    """Defining class BaseGeometry """
+
+    def area(self):
+        """area of the BaseGeometry """
+
+        raise Exception("area() is not implemented")
