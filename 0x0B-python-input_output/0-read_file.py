bb8776b5fc3e152e71fe91b08b6ebfc1bb306c5f
sylvieshimwauwase <slvshimwa@gmail.com>
2023-07-13 16:30:50 +0200

    Python Input-Output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,8 +5,5 @@
 def read_file(filename=""):
     """read utf-8 file and print its content to stdout"""
 
-    if not filename:
-        filename = ""
-    with open(filename, "r", encoding="utf-8") as file:
-        for line in file:
-            print(line)
+    with open(filename, encoding="utf-8") as file:
+        print(file.read(), end="")
