51b215b09b4ad6f4f92c920f154898aaaadbc22b
sylvieshimwauwase <slvshimwa@gmail.com>
2023-07-13 16:48:55 +0200

    Python Input-Output

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Append to a file"""
+
+
+def append_write(filename="", text=""):
+    """appends a utf-8 string to a text file"""
+
+    with open(filename, "a", encoding="utf-8") as file:
+        return file.append(text)
