3b2411a5e94a316b00b5a9da8b742a978d8a0366
sylvieshimwauwase <slvshimwa@gmail.com>
2023-07-13 16:44:33 +0200

    Python Input-Output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,4 +6,4 @@ def write_file(filename="", text=""):
     """writes utf-8 file and returns number of characters written"""
 
     with open(filename, "w", encoding="utf-8") as file:
-        return (file.write(text), end="")
+        return file.write(text)
