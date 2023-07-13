8eb010c70a5081b1ef721721cfb4338cc5aa8bd6
sylvieshimwauwase <slvshimwa@gmail.com>
2023-07-13 16:38:40 +0200

    Python Input-Output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,4 +7,4 @@ def write_file(filename="", text=""):
 
     with open(filename, encoding="utf-8") as file:
         for char in file:
-            print(file.write(text), end="")
+            print(file.write(char), end="")
