60e318d5da86ef09d506d71510f7db80ae135be4
sylvieshimwauwase <slvshimwa@gmail.com>
2023-07-21 00:05:54 +0200

    Python-Almost a Circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """creating Base class"""
 import json
+import os
 
 
 class Base:
@@ -39,7 +40,50 @@ class Base:
             if list_objs == [] or None:
                 json.dump(list_objs, filename)
             else:
-                j_str = cls.to_json_string(
-                        [obj.to_dictionary() for obj in list_objs]
-                        )
+                j_str = cls.to_json_string([object.to_dictionary() for object in list_objs])
                 f.write(j_str)
+
+    def from_json_string(json_string):
+        """method that returns list of dictionaries
+        Args:
+            json_string (str): json string
+        """
+        if json_string is None or json_string == "":
+            return []
+        return json.loads(json_string)
+
+    def create(cls, **dictionary):
+        """Class method that returns an instance with attributes already set
+        Args:
+            **dictionary: Double pointer to a dictionary representing the attributes
+        Return:
+            Instance with attributes already set
+        """
+        if cls.__name__ == "Rectangle":
+            dummy_instance = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy_instance = cls(1)
+        else:
+            dummy_instance = cls()
+
+        dummy_instance.update(**dictionary)
+        return dummy_instance
+    
+    def load_from_file(cls):
+        """Class method that returns a list of instances from a JSON file
+        Return:
+            List of instances
+        """
+        filename = cls.__name__ + ".json"
+        instance_list = []
+
+        if os.path.exists(filename):
+            with open(filename, "r") as file:
+                json_string = file.read() 
+                dict_list = json.loads(json_string)
+
+            for dictionary in dict_list:
+                instance = cls.create(**dictionary)
+                instance_list.append(instance)
+
+        return instance_list
