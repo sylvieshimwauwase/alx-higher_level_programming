02bfb6e73a623a96a505f844937a0ab4ce3b350e
sylvieshimwauwase <slvshimwa@gmail.com>
2023-08-16 20:55:28 +0200

    Introduction to Sql

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,7 @@
 -- importing database of this table
 SELECT `city`, AVG(`value`) AS`avg_temperature`
 FROM `temperatures`
-WHERE month IN (`July`, `August`)
+WHERE `month` IN (`July`, `August`)
 GROUP BY `city`
 ORDER BY `avg_temperature` DESC
 LIMIT 3;
--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,5 +1,5 @@
 -- script that displays max temperature
-SELECT `state`, MAX(`value`) AS max_temperature
+SELECT `state`, MAX(`value`) AS `max_temperature`
 FROM `temperatures`
 GROUP BY `state`
 ORDER BY `state`;
