6d7a97509e455cfaed173e9ccc6159f7a8b9859a
sylvieshimwauwase <slvshimwa@gmail.com>
2023-08-17 19:24:03 +0200

    More Queries

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,9 +4,9 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 -- script to create table
 CREATE TABLE IF NOT EXISTS cities (
-	id INT PRIMART KEY UNIQUE NOT NULL,
-	state_id INT,
-	name VARCHAR(256) NOT NULL
-
+	id INT UNIQUE AUTO_INCREMENT NOT NULL,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY(id),
 	FOREIGN KEY (state_id) REFERENCES states(id)
 );
