5bf1adae44f28b4299b018672ec7b330c6249c9f
sylvieshimwauwase <slvshimwa@gmail.com>
2023-08-18 15:29:44 +0200

    More Queries

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- scripts that lists all genres from database
-SELECT genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
+SELECT genres.name AS 'genre', COUNT(tv_show_genres.genre_id) AS 'number_of_shows'
 FROM tv_genres
 RIGHT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY genre
--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,6 +1,6 @@
 -- scripts to lists all genres of the tv show
 SELECT name
-FROM tv_shows
+FROM tv_genre
 LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_shows.title = 'Dexter'
