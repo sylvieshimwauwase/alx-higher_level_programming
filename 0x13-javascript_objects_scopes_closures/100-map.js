7152584cc6e9f23e9abdb85177b6884ed2796f87
sylvieshimwauwase <slvshimwa@gmail.com>
2023-09-12 20:25:54 +0200

    Javascript - objects, scopes and closures

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,2 @@
 #!/usr/bin/node
-const { list } = require('./100-data');
-
-const newList = list.map((value, index) => value * index);
-
-console.log('Initial list:', list);
-console.log('New list:', newList);
+exports.list = [1, 2, 3, 4, 5];
