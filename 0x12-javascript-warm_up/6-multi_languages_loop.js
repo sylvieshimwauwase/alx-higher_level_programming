2fba420ebbf7f8adbe10d263ebd75b8df7cbba64
sylvieshimwauwase <slvshimwa@gmail.com>
2023-09-12 10:56:55 +0200

    javascript

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const txt = ['C is fun", "Python is cool", "JavaScript is amazing'];
+const txt = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < txt.length; i++) {
    console.log(txt[i]);
