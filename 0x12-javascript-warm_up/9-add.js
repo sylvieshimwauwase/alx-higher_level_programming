903f016ab00eadf204bd1c7e3ae9030893dbaa19
sylvieshimwauwase <slvshimwa@gmail.com>
2023-09-12 11:09:18 +0200

    javascript

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,15 +1,10 @@
 #!/usr/bin/node
 
-function add (a, b) {
-  return a + b;
-}
-
 const arg1 = parseInt(process.argv[2]);
 const arg2 = parseInt(process.argv[3]);
 
-if (!isNaN(arg1) && !isNaN(arg2)) {
-  const result = add(arg1, arg2);
-  console.log(result);
-} else {
-  console.log('Invalid input. Please provide two integer arguments.');
+function add (arg1, arg2) {
+  return (arg1 + arg2);
 }
+
+console.log(add(arg1, arg2));
