526ff8796e8ccbc4577943b39c1da0b3667a25cb
sylvieshimwauwase <slvshimwa@gmail.com>
2023-09-15 17:10:10 +0200

    Python Object Relational Mapping

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -21,7 +21,9 @@ if __name__ == '__main__':
     cursor = db.cursor()
 
     cursor.execute(
-            "SELECT * FROM cities ORDER BY id ASC"
+            "SELECT cities.id, cities.name, states.name \
+                    FROM cities JOIN states ON cities.state_id \
+                    = states.id ORDER BY cities.id ASC"
             )
 
     cities = cursor.fetchall()
