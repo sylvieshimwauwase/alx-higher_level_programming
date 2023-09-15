0f4f583af929865ce92fd010e05592117c842d00
sylvieshimwauwase <slvshimwa@gmail.com>
2023-09-15 16:10:18 +0200

    Python Object Relational Mapping

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -18,7 +18,8 @@ if __name__ == '__main__':
 
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cursor.execute(
+            "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
 
     states = cursor.fetchall()
 
