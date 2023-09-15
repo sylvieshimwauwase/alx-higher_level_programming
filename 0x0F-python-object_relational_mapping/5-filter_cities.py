0eed49e8827f637368d56c81c654c3ae4e338cb3
sylvieshimwauwase <slvshimwa@gmail.com>
2023-09-15 17:15:10 +0200

    Python Object Relational Mapping

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+script to list all cities from database
+where name matches the argument
+from the database 'hbtn_0e_4_usa'
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """
+    from database get cities
+    """
+
+    db = MySQLdb.connect(
+            host="localhost", user=argv[1], port=3306,
+            passwd=argv[2], db=argv[3]
+            )
+
+    cursor = db.cursor()
+
+    cursor.execute(
+            "SELECT cities.id, cities.name, states.name \
+                    FROM cities JOIN states ON cities.state_id \
+                    = states.id WHERE states.name LIKE BINARY \
+                    %(states.name)s ORDER BY cities.id ASC"
+            )
+
+    cities = cursor.fetchall()
+
+    if cities is not None:
+        for city in cities:
+            print(city)
