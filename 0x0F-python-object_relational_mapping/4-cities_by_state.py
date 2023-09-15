f0d44e404b590049ed722c8eadd017432b7f4056
sylvieshimwauwase <slvshimwa@gmail.com>
2023-09-15 16:54:23 +0200

    Python Object Relational Mapping

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+script to list all cities from database
+where name matches the argument
+from the database 'hbtn_0e_4_usa'
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """
+    from database get cities
+    """
+
+    db = MySQLdb.connect(
+            host="localhost", user=argv[1], port=3306,
+            passwd=argv[2], db=argv[3]
+            )
+
+    cursor = db.cursor()
+
+    cursor.execute(
+            "SELECT * FROM cities ORDER BY id ASC"
+            )
+
+    cities = cursor.fetchall()
+
+    for city in cities:
+        print(city)
+
+    sys.exit(0)
