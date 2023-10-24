869673855a8cf76d734844cb6e28fca051ac9141
sylvieshimwauwase <slvshimwa@gmail.com>
2023-10-24 23:40:13 +0200

    JavaScript-Web scraping

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = 'https://jsonplaceholder.typicode.com/todos';
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else if (response.statusCode === 200) {
+    const completed = {};
+    const tasks = JSON.parse(body);
+    for (const a in tasks) {
+      const task = tasks[a];
+      if (task.completed === true) {
+        if (completed[task.userId] === undefined) {
+          completed[task.userId] = 1;
+        } else {
+          completed[task.userId]++;
+        }
+      }
+    }
+    console.log(completed);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
+  }
+});
