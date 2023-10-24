ef13b7ca5fa62a47a9fe01e14c2572654d96cd5c
sylvieshimwauwase <slvshimwa@gmail.com>
2023-10-24 23:50:49 +0200

    JavaScript-Web scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,10 +3,10 @@
 const fs = require('fs');
 const filename = process.argv[2];
 
-fs.readfile(filename, 'utf-8', (err, data) => {
-  if (err) {
-    console.log(err);
+fs.readfile(filename, 'utf-8', (error, content) => {
+  if (error) {
+    console.log(error);
   } else {
-    console.log(data);
+    console.log(content);
   }
 });
