a02cb95ad87be53891a7f038d0eed7dfc8b40837
sylvieshimwauwase <slvshimwa@gmail.com>
2023-10-25 00:13:01 +0200

    JavaScript-Web scraping

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,7 +3,7 @@
 const request = require('request');
 const url = 'https://jsonplaceholder.typicode.com/todos';
 
-request(url, (error, response, body) => {
+request(url, function (error, response, body) {
   if (error) {
     console.log(error);
   } else if (response.statusCode === 200) {
